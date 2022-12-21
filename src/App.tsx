7c2c5dc3b6295e410c8fb2cbcf2f125349406404
Joe Palmeras <joepalmeras-github@web.de>
2022-12-21 13:04:12 +0100

    TASK-8 Implements 'WrapperApp'

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-
-import { Provider } from "jotai";
-import { Suspense } from 'react';
 import { SelectCountry } from "./components/dropdown/SelectCountry";
 import { RandomUserTable } from "./components/RandomUserTable";
 
 const App = () => {
   return (
     <>
-      <Provider>
-        <Suspense fallback={<div>Loading Data</div>}>
-          <SelectCountry />
-          <RandomUserTable />
-        </Suspense>
-      </Provider>
+      <SelectCountry />
+      <RandomUserTable />
     </>
   );
 }
