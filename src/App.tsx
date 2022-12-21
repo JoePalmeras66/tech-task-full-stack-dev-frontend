22e5b2488314b7e36b1c0a9cfd86339e35b88596
Joe Palmeras <joepalmeras-github@web.de>
2022-12-21 12:46:31 +0100

    TASK-7 - Infinite loop when Suspense contains a Provider with initialValues set

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 
 import { Provider } from "jotai";
+import { Suspense } from 'react';
 import { SelectCountry } from "./components/dropdown/SelectCountry";
+import { RandomUsersList } from './components/RandomUsersList';
 
 const App = () => {
   return (
     <>
       <Provider>
-        <SelectCountry />
+        <Suspense fallback={<div>Loading something</div>}>
+          <SelectCountry />
+          <RandomUsersList />
+        </Suspense>
       </Provider>
     </>
   );
