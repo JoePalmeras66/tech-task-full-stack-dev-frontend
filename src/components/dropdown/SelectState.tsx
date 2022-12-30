307db987f502147a4752d48362649edb49a5cd61
Joe Palmeras <joepalmeras-github@web.de>
2022-12-30 18:03:02 +0100

    TASK-18 Replace DropDown 'Select' from npm package 'rc-select' with own DropDown 'GenericCustomSelect'

--- src/components/dropdown/SelectState.tsx
+++ src/components/dropdown/SelectState.tsx
@@ -1,22 +1,18 @@
 import { useAtom } from "jotai";
-import { filterStateAtom, sortedStatesAtom } from "../../services/state-service";
-import Select from "rc-select";
+import { sortedStatesAtom } from "../../services/state-service";
+import GenericCustomSelect from "./GenericCustomSelect";
+import { filterStateAtom } from "../../services/randomusers-service";
 
 export const SelectState = () => {
     const [state, setState] = useAtom(filterStateAtom);
     const [states] = useAtom(sortedStatesAtom);
-    console.log("States: "+states)
     return (
         <>
-            <div style={{ width: 300 }}>
-                <Select
-                    className='rc-select'
-                    value={state}
-                    onChange={setState}
-                >
-                    {states}
-                </Select>
-            </div>
+           <GenericCustomSelect 
+                value={state} 
+                onChange={setState} 
+                options={states}
+            />
         </>
     )
 }
