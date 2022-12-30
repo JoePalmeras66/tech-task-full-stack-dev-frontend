a1bee8c2fd4ec61c2676ca74aef12f846a6b0beb
Joe Palmeras <joepalmeras-github@web.de>
2022-12-30 19:21:35 +0100

    TASK-20 Improve Code Readability

--- src/components/layouts/TechTaskResponsiveGridLayout.tsx
+++ src/components/layouts/TechTaskResponsiveGridLayout.tsx
@@ -3,14 +3,16 @@ import { Layouts, Responsive, WidthProvider } from "react-grid-layout";
 import { SelectCountry } from "../dropdown/SelectCountry";
 import { RandomUsersTable } from "../table/RandomUsersTable";
 import { SelectState } from "../dropdown/SelectState";
+import { SelectCity } from "../dropdown/SelectCity";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export const TechTaskResponsiveGridLayout = () => {
     const myLayouts: Layouts = {
-        xs: [{ i: "1", x: 0, y: 0, w: 2, h: 1, minW: 2, minH: 1 }],
-        md: [{ i: "2", x: 0, y: 0, w: 2, h: 1, minW: 2, minH: 1 }],
-        lg: [{ i: "3", x: 10, y: 0, w: 2, h: 1, minW: 2, minH: 1 }]
+        countriesDD: [{ i: "1", x: 0, y: 0, w: 2, h: 1, minW: 2, minH: 1 }],
+        statesDD: [{ i: "2", x: 0, y: 0, w: 2, h: 1, minW: 2, minH: 1 }],
+        citiesDD: [{ i: "3", x: 0, y: 0, w: 2, h: 1, minW: 2, minH: 1 }],
+        randomusersT: [{ i: "4", x: 10, y: 0, w: 2, h: 1, minW: 2, minH: 1 }]
     }
 
     const [layouts] = useState<Layouts>(myLayouts);
@@ -19,12 +21,13 @@ export const TechTaskResponsiveGridLayout = () => {
         <ResponsiveGridLayout
             className="layout"
             layouts={layouts}
-            breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-            cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+            breakpoints={{ randomusersT: 1200, countriesDD: 480, statesDD: 480, citiesDD: 480 }}
+            cols={{ randomusersT: 12, countriesDD: 2, statesDD: 2, citiesDD: 2 }}
         >
             <div key="1"><SelectCountry /></div>
             <div key="2"><SelectState /></div>
-            <div key="3"><RandomUsersTable /></div>
+            <div key="3"><SelectCity /></div>
+            <div key="4"><RandomUsersTable /></div>
         </ResponsiveGridLayout>
       </>
     );
